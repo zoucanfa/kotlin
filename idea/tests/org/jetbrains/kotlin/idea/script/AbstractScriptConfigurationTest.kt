ba5c44bbc90cf8dd9aa7fa18335f8a60bd79fac7
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2016-11-18 14:48:49 +0100

    Fix test code after UsefulTestCase api changes

--- idea/tests/org/jetbrains/kotlin/idea/script/AbstractScriptConfigurationTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/script/AbstractScriptConfigurationTest.kt
@@ -126,7 +126,7 @@ abstract class AbstractScriptConfigurationTest : AbstractPsiCheckerTest() {
                 Extensions.getArea(project),
                 ScriptTemplatesProvider.EP_NAME,
                 provider,
-                myTestRootDisposable
+                testRootDisposable
         )
         KotlinScriptConfigurationManager.reloadScriptDefinitions(project)
     }
