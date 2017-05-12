482384be78d5964b873443a033e29b0b7a21b5ca
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-05-12 13:30:08 +0200

    more unification with lib/tools build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,42 @@ import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.internal.HasConvention
 import org.gradle.api.tasks.testing.Test
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 extra["kotlinVersion"] = extra["kotlin_version"]
 extra["build.number"] = "1.1-SNAPSHOT"
+
+extra["kotlin_root"] = rootDir
+
+buildscript {
+    extra["kotlin_version"] = project.properties["deployVersion"] ?: "1.1-SNAPSHOT"
+    extra["kotlin_language_version"] = "1.1"
+    extra["kotlin_gradle_plugin_version"] = "1.1-SNAPSHOT"
+    extra["repo"] = "https://repo.gradle.org/gradle/repo"
+
+    repositories {
+        mavenLocal()
+        maven { setUrl(rootProject.extra["repo"]) }
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+    }
+}
+
+plugins {
+    java // so we can benefit from the `java()` accessor below
+}
+
+val bootstrapCfg = configurations.create("bootstrap")
+val scriptCompileCfg = configurations.create("scriptCompile")
+val scriptRuntimeCfg = configurations.create("scriptRuntime").extendsFrom(scriptCompileCfg)
+
+dependencies {
+    bootstrapCfg(kotlinDep("compiler-embeddable"))
+    scriptCompileCfg(kotlinDep("compiler-embeddable"))
+}
 
 val distDir = "$rootDir/build/dist"
 val distLibDir = "$distDir/kotlinc/lib"
@@ -34,7 +67,7 @@ extra["JDK_18"] = jdkPath("1.8")
 extra["compilerJar"] = project.file("$distLibDir/kotlin-compiler.jar")
 extra["embeddableCompilerJar"] = project.file("$distLibDir/kotlin-compiler-embeddable.jar")
 extra["compilerJarWithBootstrapRuntime"] = project.file("$distDir/kotlin-compiler-with-bootstrap-runtime.jar")
-extra["bootstrapCompilerFile"] = project.file("$distDir/kotlin-compiler-with-bootstrap-runtime.jar")
+extra["bootstrapCompilerFile"] = bootstrapCfg.files.first().canonicalPath
 
 extra["versions.protobuf-java"] = "2.6.1"
 extra["versions.javax.inject"] = "1"
@@ -44,25 +77,7 @@ extra["versions.jansi"] = "1.11"
 extra["versions.jline"] = "2.12.1"
 extra["versions.javaslang"] = "2.0.6"
 
-buildscript {
-    extra["kotlin_version"] = project.properties["deployVersion"] ?: "1.1-SNAPSHOT"
-    extra["kotlin_language_version"] = "1.1"
-    extra["kotlin_gradle_plugin_version"] = "1.1-SNAPSHOT"
-    extra["repo"] = "https://repo.gradle.org/gradle/repo"
-
-    repositories {
-        mavenLocal()
-        maven { setUrl(rootProject.extra["repo"]) }
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
-    }
-}
-
-plugins {
-    java // so we can benefit from the `java()` accessor below
-}
+applyFrom("libraries/commonConfiguration.gradle")
 
 val importedAntTasksPrefix = "imported-ant-update-"
 
@@ -79,6 +94,10 @@ tasks.matching { task ->
 //    dependsOn(tasks.getByName(importedAntTasksPrefix + "update"))
 //}
 
+val prepareBootstrapTask = task("prepareBootstrap") {
+    dependsOn(bootstrapCfg, scriptCompileCfg, scriptRuntimeCfg)
+}
+
 allprojects {
     setBuildDir("$rootDir/build/${project.name}")
 
@@ -87,41 +106,52 @@ allprojects {
         maven { setUrl(rootProject.extra["repo"]) }
         mavenCentral()
     }
-}
 
-//subprojects {
-//
-//    task<Jar>("javadocJar") {
-//        classifier = "javadoc"
-//    }
-//}
+    tasks.withType<KotlinCompile> {
+        dependsOn(prepareBootstrapTask)
+//        println(rootProject.extra["bootstrapCompilerFile"].toString())
+        compilerJarFile = File(rootProject.extra["bootstrapCompilerFile"].toString())
+    }
 
-task("preparePublication") {
-    val repositoryProviders = mapOf("sonatype-nexus-staging" to "sonatype", "sonatype-nexus-snapshots" to "sonatype")
-
-    val isRelease = !project.version.toString().contains("-SNAPSHOT")
-    project.extra["isRelease"] = isRelease
-
-    val repo = properties["deploy-repo"] as? String
-    val repoProvider = repo?.let { repositoryProviders.getOrDefault(it, it) }
-    project.extra["isSonatypePublish"] = repoProvider == "sonatype"
-    val isSonatypePublish = repoProvider == "sonatype"
-    project.extra["isSonatypePublish"] = isSonatypePublish
-    project.extra["isSonatypeRelease"] = isSonatypePublish && isRelease
-
-    project.extra["signing.keyId"] = properties["kotlin.key.name"]
-    project.extra["signing.password"] = properties["kotlin.key.passphrase"]
-
-    val sonatypeSnapshotsUrl = if (isSonatypePublish && !isRelease) "https://oss.sonatype.org/content/repositories/snapshots/" else null
-
-    extra["repoUrl"] = properties["deployRepoUrl"] ?: sonatypeSnapshotsUrl ?: properties["deploy-url"] ?: "file://${rootProject.buildDir}/repo"
-    extra["username"] = properties["deployRepoUsername"] ?: properties["kotlin.${repoProvider}.user"]
-    extra["password"] = properties["deployRepoPassword"] ?: properties["kotlin.${repoProvider}.password"]
-
-    doLast {
-        println("Deployment respository url: ${extra["repoUrl"]}")
+    tasks.withType<Kotlin2JsCompile> {
+        dependsOn(prepareBootstrapTask)
+        compilerJarFile = File(rootProject.extra["bootstrapCompilerFile"].toString())
     }
 }
+
+subprojects {
+
+    task<Jar>("javadocJar") {
+        classifier = "javadoc"
+    }
+}
+
+//task("preparePublication") {
+//    val repositoryProviders = mapOf("sonatype-nexus-staging" to "sonatype", "sonatype-nexus-snapshots" to "sonatype")
+//
+//    val isRelease = !project.version.toString().contains("-SNAPSHOT")
+//    project.extra["isRelease"] = isRelease
+//
+//    val repo = properties["deploy-repo"] as? String
+//    val repoProvider = repo?.let { repositoryProviders.getOrDefault(it, it) }
+//    project.extra["isSonatypePublish"] = repoProvider == "sonatype"
+//    val isSonatypePublish = repoProvider == "sonatype"
+//    project.extra["isSonatypePublish"] = isSonatypePublish
+//    project.extra["isSonatypeRelease"] = isSonatypePublish && isRelease
+//
+//    project.extra["signing.keyId"] = properties["kotlin.key.name"]
+//    project.extra["signing.password"] = properties["kotlin.key.passphrase"]
+//
+//    val sonatypeSnapshotsUrl = if (isSonatypePublish && !isRelease) "https://oss.sonatype.org/content/repositories/snapshots/" else null
+//
+//    extra["repoUrl"] = properties["deployRepoUrl"] ?: sonatypeSnapshotsUrl ?: properties["deploy-url"] ?: "file://${rootProject.buildDir}/repo"
+//    extra["username"] = properties["deployRepoUsername"] ?: properties["kotlin.${repoProvider}.user"]
+//    extra["password"] = properties["deployRepoPassword"] ?: properties["kotlin.${repoProvider}.password"]
+//
+//    doLast {
+//        println("Deployment respository url: ${extra["repoUrl"]}")
+//    }
+//}
 
 fun jdkPath(version: String): String {
     val varName = "JDK_${version.replace(".", "")}"
