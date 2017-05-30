74bfdea3041fe8de2dffa0c1ebe831fa44d14622
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-05-30 18:20:41 +0200

    fix ant tests

--- ant/build.gradle.kts
+++ ant/build.gradle.kts
@@ -17,6 +17,9 @@ val jar: Jar by tasks
 jar.apply {
     setupRuntimeJar("Kotlin Ant Tools")
     archiveName = "kotlin-ant.jar"
+    from("$projectDir/src") { include("**/*.xml") }
+
+    manifest.attributes.put("Class-Path", "kotlin-stdlib.jar kotlin-reflect.jar kotlin-script-runtime.jar kotlin-preloader.jar")
 }
 
 dist {
