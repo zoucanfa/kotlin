6411839cf6dbbb160e0238ed1fc31558245f5850
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-06-01 16:47:01 +0300

    Fix internal compiler error in DefaultParameterValueSubstitutor
    Most likely a typo in the original code.

--- compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/diagnostics/JvmDeclarationOrigin.kt
+++ compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/diagnostics/JvmDeclarationOrigin.kt
@@ -60,16 +60,16 @@ fun OtherOrigin(element: PsiElement?, descriptor: DeclarationDescriptor?): JvmDe
         else JvmDeclarationOrigin(OTHER, element, descriptor)
 
 fun OtherOrigin(element: KtPureElement?, descriptor: DeclarationDescriptor?): JvmDeclarationOrigin =
-        OtherOrigin(element?.psiOrParent as PsiElement, descriptor)
+        OtherOrigin(element?.psiOrParent as PsiElement?, descriptor)
 
-fun OtherOrigin(element: KtElement, descriptor: DeclarationDescriptor?): JvmDeclarationOrigin =
-        OtherOrigin(element as PsiElement, descriptor)
+fun OtherOrigin(element: KtElement?, descriptor: DeclarationDescriptor?): JvmDeclarationOrigin =
+        OtherOrigin(element as PsiElement?, descriptor)
 
-fun OtherOrigin(element: PsiElement): JvmDeclarationOrigin = OtherOrigin(element, null)
+fun OtherOrigin(element: PsiElement?): JvmDeclarationOrigin = OtherOrigin(element, null)
 
-fun OtherOrigin(element: KtPureElement): JvmDeclarationOrigin = OtherOrigin(element, null)
+fun OtherOrigin(element: KtPureElement?): JvmDeclarationOrigin = OtherOrigin(element, null)
 
-fun OtherOrigin(element: KtElement): JvmDeclarationOrigin = OtherOrigin(element, null)
+fun OtherOrigin(element: KtElement?): JvmDeclarationOrigin = OtherOrigin(element, null)
 
 fun OtherOrigin(descriptor: DeclarationDescriptor): JvmDeclarationOrigin = JvmDeclarationOrigin(OTHER, null, descriptor)
 
