ad572a8a0d3df1623c3b742e2b9dbbee911ceb8d
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-06-05 19:12:49 +0300

    Android Extensions, minor: rename AndroidClassType to AndroidEntityType

--- plugins/android-extensions/android-extensions-compiler/src/org/jetbrains/kotlin/android/synthetic/codegen/AndroidOnDestroyClassBuilderInterceptorExtension.kt
+++ plugins/android-extensions/android-extensions-compiler/src/org/jetbrains/kotlin/android/synthetic/codegen/AndroidOnDestroyClassBuilderInterceptorExtension.kt
@@ -115,7 +115,7 @@ class AndroidOnDestroyClassBuilderInterceptorExtension : ClassBuilderInterceptor
 
                     val container = bindingContext.get(BindingContext.CLASS, currentClass) ?: return
                     val entityOptions = AndroidEntityOptionsProxy.get(container)
-                    if (!entityOptions.classType.isFragment || entityOptions.cache == NO_CACHE) return
+                    if (!entityOptions.entityType.isFragment || entityOptions.cache == NO_CACHE) return
 
                     val iv = InstructionAdapter(this)
                     iv.load(0, classType)
