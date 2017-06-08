352ad12e32021d48de28dc1d39201b5a3ec37df8
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-06-08 15:38:10 +0300

    SamWithReceiver: Add general-purpose plugin and Gradle/Maven integrations

--- libraries/tools/kotlin-maven-plugin-test/src/it/test-sam-with-receiver-simple/src/main/java/lib/Manager.java
+++ libraries/tools/kotlin-maven-plugin-test/src/it/test-sam-with-receiver-simple/src/main/java/lib/Manager.java
@@ -2,5 +2,9 @@ package lib;
 
 public class Manager {
     public void doJob(Job job) {}
+<<<<<<< HEAD
     public void doJobWithoutReceiver(JobWithoutReceiver job) {}
+=======
+    public void doJobWithoutReceiver(lib.JobWithoutReceiver job) {}
+>>>>>>> b235eaed517... SamWithReceiver: Add general-purpose plugin and Gradle/Maven integrations
 }
