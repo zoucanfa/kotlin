4e687ab40e3e3493059133d49c173a0c190f7a15
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-06-20 16:52:49 +0300

    Kapt3, minor, tests: Weaken regular expression for Metadata shrinking

--- plugins/kapt3/testData/converter/dataClass.kt
+++ plugins/kapt3/testData/converter/dataClass.kt
@@ -1,9 +1,3 @@
-import java.util.Date
-
 data class User(val firstName: String, val secondName: String, val age: Int) {
     fun procedure() {}
 }
-
-annotation class PrimaryKey
-
-data class Task (@field:PrimaryKey val uuid: String, val text: String, val updatedAt: Date)
