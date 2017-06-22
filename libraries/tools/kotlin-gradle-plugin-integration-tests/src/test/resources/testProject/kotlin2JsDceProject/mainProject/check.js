4eae5141bcae86fc460eb7c509235fa273597605
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-06-22 13:41:34 +0300

    ~fixup 10406869ec8ba2c922140a6d55085f3ab5b15e12: change DCE task name, remove unnecessary comment from check.js

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kotlin2JsDceProject/mainProject/check.js
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kotlin2JsDceProject/mainProject/check.js
@@ -1,6 +1,5 @@
 (function() {
     var status = exampleapp.exampleapp.status;
-    //java.lang.System.out.println(Object.keys(exampleapp.exampleapp).toString());
     if (status !== "foo") {
         throw new Error("Unexpected status: " + status);
     }
