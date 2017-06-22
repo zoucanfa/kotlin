021fa39630d7d46af6993d76ec7c146aa27ef011
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-06-23 00:33:42 +0300

    ~ api v4

--- core/script.runtime/src/kotlin/script/templates/annotations.kt
+++ core/script.runtime/src/kotlin/script/templates/annotations.kt
@@ -19,14 +19,13 @@
 package kotlin.script.templates
 
 import kotlin.reflect.KClass
-import kotlin.script.dependencies.EmptyDependenciesResolver
 import kotlin.script.dependencies.ScriptDependenciesResolver
 
 const val DEFAULT_SCRIPT_FILE_PATTERN = ".*\\.kts"
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ScriptTemplateDefinition(val resolver: KClass<out ScriptDependenciesResolver> = EmptyDependenciesResolver::class,
+annotation class ScriptTemplateDefinition(val resolver: KClass<out ScriptDependenciesResolver> = ScriptDependenciesResolver.Empty::class,
                                           val scriptFilePattern: String = DEFAULT_SCRIPT_FILE_PATTERN)
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
