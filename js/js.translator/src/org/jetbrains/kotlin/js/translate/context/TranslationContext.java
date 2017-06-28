1d1fee8462ab352529bdc0296d61240d3a926c6a
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-06-28 13:16:14 +0300

    Introduce new JS AST metadata property
    Add a new metadata property to JsName that points from
    local import alias in inline function to local declaration.
    This is required when inlining public function from current module
    that calls another function from the same module. In this case
    we want to avoid "importing" declaration we already having locally.

--- js/js.translator/src/org/jetbrains/kotlin/js/translate/context/TranslationContext.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/translate/context/TranslationContext.java
@@ -395,6 +395,9 @@ public class TranslationContext {
             String tag = staticContext.getTag(descriptor);
             name = inlineFunctionContext.getImports().computeIfAbsent(tag, t -> {
                 JsName result = JsScope.declareTemporaryName(StaticContext.getSuggestedName(descriptor));
+                if (isFromCurrentModule(descriptor) && !AnnotationsUtils.isNativeObject(descriptor)) {
+                    MetadataProperties.setLocalAlias(result, getInnerNameForDescriptor(descriptor));
+                }
                 JsExpression imported = createInlineLocalImportExpression(descriptor);
                 inlineFunctionContext.getImportBlock().getStatements().add(JsAstUtils.newVar(result, imported));
                 return result;
