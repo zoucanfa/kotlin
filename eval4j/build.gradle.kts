1d9769ce9a213d763b0c95559775a513bb542093
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-07-04 13:54:30 +0200

    Add j2k and eval4j tests

--- eval4j/build.gradle.kts
+++ eval4j/build.gradle.kts
@@ -10,9 +10,19 @@ dependencies {
 //    compile(files(PathUtil.getJdkClassesRootsFromCurrentJre())) // TODO: make this one work instead of the nex one, since it contains more universal logic
     compile(files("${System.getProperty("java.home")}/../lib/tools.jar"))
     buildVersion()
+    testCompile(project(":idea:idea-test-framework"))
+    testCompile(commonDep("junit:junit"))
+    testCompile(project(":kotlin-test:kotlin-test-jvm"))
 }
 
 configureKotlinProjectSourcesDefault()
-configureKotlinProjectNoTests()
+configureKotlinProjectTestsDefault()
+
+tasks.withType<Test> {
+    workingDir = rootDir
+    systemProperty("idea.is.unit.test", "true")
+    environment("NO_FS_ROOTS_ACCESS_CHECK", "true")
+    ignoreFailures = true
+}
 
 fixKotlinTaskDependencies()
