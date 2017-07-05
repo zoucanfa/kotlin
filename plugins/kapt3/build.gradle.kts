8429b9a5844ca8bcb0efa2488125f270df4eed33
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-07-05 10:45:36 +0200

    Add kapt3

---
+++ plugins/kapt3/build.gradle.kts
@@ -0,0 +1,25 @@
+
+apply { plugin("kotlin") }
+
+dependencies {
+    compile(project(":compiler:util"))
+    compile(project(":compiler:cli"))
+    compile(project(":compiler:backend"))
+    compile(project(":compiler:frontend"))
+    compile(project(":compiler:frontend.java"))
+    compile(project(":compiler:plugin-api"))
+    testCompile(project(":compiler.tests-common"))
+    testCompile(commonDep("junit:junit"))
+}
+
+configureKotlinProjectSourcesDefault()
+configureKotlinProjectTestsDefault()
+
+tasks.withType<Test> {
+    workingDir = rootDir
+    systemProperty("idea.is.unit.test", "true")
+    environment("NO_FS_ROOTS_ACCESS_CHECK", "true")
+    ignoreFailures = true
+}
+
+fixKotlinTaskDependencies()
