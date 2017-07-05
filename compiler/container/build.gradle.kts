dab3d84101fc34928686b588a9162d03ba3e5908
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-07-05 12:05:31 +0200

    Add compiler:container tests

--- compiler/container/build.gradle.kts
+++ compiler/container/build.gradle.kts
@@ -5,9 +5,22 @@ dependencies {
     compile(project(":core:util.runtime"))
     compile(commonDep("javax.inject"))
     compile(ideaSdkCoreDeps("intellij-core"))
+    testCompile(commonDep("junit:junit"))
+    testCompile(project(":kotlin-test:kotlin-test-jvm"))
+    testRuntime(ideaSdkCoreDeps("trove4j", "intellij-core"))
 }
 
 configureKotlinProjectSourcesDefault()
 configureKotlinProjectTestsDefault()
 
 fixKotlinTaskDependencies()
+
+tasks.withType<Test> {
+    dependsOnTaskIfExistsRec("dist", project = rootProject)
+    dependsOn(":prepare:mock-runtime-for-test:dist")
+    dependsOn(":prepare:compiler:prepare")
+    workingDir = rootDir
+    systemProperty("idea.is.unit.test", "true")
+    environment("NO_FS_ROOTS_ACCESS_CHECK", "true")
+    ignoreFailures = true
+}
