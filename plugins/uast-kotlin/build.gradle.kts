37b2d4093b925ddeae45d328dd2b230255d2b5cb
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-07-05 11:15:56 +0200

    Add uast-kotlin tests

--- plugins/uast-kotlin/build.gradle.kts
+++ plugins/uast-kotlin/build.gradle.kts
@@ -10,9 +10,36 @@ dependencies {
     compile(project(":compiler:light-classes"))
     compile(preloadedDeps("uast-common", "uast-java"))
     buildVersion()
+    testCompile(project(":compiler.tests-common"))
+    testCompile(commonDep("junit:junit"))
+    testCompile(project(":compiler:util"))
+    testCompile(project(":compiler:cli"))
+    testCompile(project(":idea:idea-android"))
+    testCompile(preloadedDeps("uast-tests"))
+    testRuntime(ideaSdkDeps("*.jar"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/java-i18n/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/properties/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/gradle/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/junit/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/IntelliLang/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/testng/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/copyright/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/properties/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/java-decompiler/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/Groovy/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/maven/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/coverage/lib"))
+    testRuntime(ideaSdkDeps("*.jar", subdir = "plugins/android/lib"))
 }
 
 configureKotlinProjectSourcesDefault()
-configureKotlinProjectNoTests()
+configureKotlinProjectTestsDefault()
 
 fixKotlinTaskDependencies()
+
+tasks.withType<Test> {
+    workingDir = rootDir
+    systemProperty("idea.is.unit.test", "true")
+    environment("NO_FS_ROOTS_ACCESS_CHECK", "true")
+    ignoreFailures = true
+}
